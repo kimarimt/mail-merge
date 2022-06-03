cb8c415e65db24f23fdcb00021a2faf89ee68191
Alec German <kimarimt@protonmail.com>
2022-06-03 13:47:28 -0400

    implemented the mail merge program

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+def get_text():
+    letter_path = 'Input/Letters/starting_letter.txt'
+    with open(letter_path, 'r') as f:
+        data = f.read()
+        f.close()
+        return data
+
+
+def get_names():
+    names_path = 'Input/Names/invited_names.txt'
+    with open(names_path, 'r') as f:
+        data = f.read()
+        f.close()
+        return data.splitlines()
+
+
 def main():
-    print('Hello, World!')
+    output_path = 'Output/ReadyToSend'
+    text = get_text()
+    names = get_names()
+
+    for name in names:
+        with open(f'{output_path}/letter_for_{name}.txt', 'w') as f:
+            letter = text.replace('[name]', name)
+            f.write(letter)
+            f.close()
+
+    print('Letter were successfully saved')
 
 
 if __name__ == '__main__':
